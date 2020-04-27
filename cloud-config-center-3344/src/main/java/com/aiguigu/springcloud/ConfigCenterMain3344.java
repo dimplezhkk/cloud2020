2e07b37f4d9fae092e551cb8a816b2ff891f6e32
ZH <ZH@zh.com>
2020-04-27 21:31:53 +0800

    修改提交

---
+++ cloud-config-center-3344/src/main/java/com/aiguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,17 @@
+package com.aiguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @Author 郑豪
+ * @Date 2020/4/27 19:18
+ **/
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class,args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Author 郑豪
+ * @Date 2020/4/27 20:34
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}
